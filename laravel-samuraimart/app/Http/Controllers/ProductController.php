07ad478518f23c6082ff531c9285ff10c7dc866c
takuya_tominaga <tom.eng.pg.03tw@gmail.com>
2023-03-20 22:29:12 +0900

    2削除

--- laravel-samuraimart/app/Http/Controllers/ProductController.php
+++ laravel-samuraimart/app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
     {
         $products = Product::all();
 
-        return view('products.index2', compact('products'));
+        return view('products.index', compact('products'));
     }
 
     /**
